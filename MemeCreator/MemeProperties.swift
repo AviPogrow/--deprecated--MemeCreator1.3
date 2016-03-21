5f28e52d187ee27c62326f98d9ce39e8a7c23bcb
Avi Pogrow <rpogrow@gmail.com>
2016-03-21 07:55:51 -0400

    Initial Commit

---
+++ MemeCreator/MemeProperties.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeCreator
+//
+//  Created by new on 3/1/16.
+//  Copyright © 2016 Avi Pogrow. All rights reserved.
+//
+import UIKit
+import CoreData
+
+extension Meme  {
+	
+	@NSManaged var topField:String?
+	@NSManaged var bottomField:String?
+	@NSManaged var compositeImageID: NSNumber?
+}
