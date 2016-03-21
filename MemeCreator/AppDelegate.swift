d9ab18ae591d3935a9269d7e2155d8eecb50522b
Avi Pogrow <rpogrow@gmail.com>
2016-03-21 15:15:41 -0400

    add sharedContextSingleton to appDelegate

--- MemeCreator/AppDelegate.swift
+++ MemeCreator/AppDelegate.swift
@@ -13,6 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 	
+	lazy var sharedContext = {
+		CoreDataStackManager.sharedInstance().managedObjectContext
+	
+	}()
+	
+	
+	
 	//customize color of UI components
 	func customizeAppearance() {
 		//make nav bar black and its text white
@@ -43,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     do {
       let jsonArray = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: .AllowFragments) as! NSArray
-      let entity = NSEntityDescription.entityForName("Team", inManagedObjectContext: coreDataStack.context)
+      let entity = NSEntityDescription.entityForName("Team", inManagedObjectContext: self.sharedContext)
       
       for jsonDictionary in jsonArray {
         let teamName = jsonDictionary["teamName"] as! String
@@ -52,35 +59,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let wins = jsonDictionary["wins"] as! NSNumber
         
         let team = Team(entity: entity!,
-          insertIntoManagedObjectContext: coreDataStack.context)
+          insertIntoManagedObjectContext: self.sharedcontext)
         team.teamName = teamName
         team.imageName = imageName
         team.qualifyingZone = zone
         team.wins = wins
       }
       
-      coreDataStack.saveContext()
-      print("Imported \(jsonArray.count) teams")
-      
-    } catch let error as NSError {
-      print("Error importing teams: \(error)")
+      do {
+        try sharedContext.save()
+      } catch {
+        print("error saving to context\(error)")
+      }
     }
   }	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	func applicationWillResignActive(application: UIApplication) {
+func applicationWillResignActive(application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	}
